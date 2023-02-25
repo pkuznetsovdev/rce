e00ec911e81667286266ceb5f261b27aff71a607
Pavel Kuznetsov <kuznetsov.pavel@protonmail.com>
2023-02-25 20:52:46 +0700

    refactor: types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,34 @@ import { ContentElements } from "src/content-elements";
 import { useGetData } from "src/data";
 import { CustomComponents } from "src/custom";
 
+interface CEConfigByNameMap {
+  'text': 'Text Props Config';
+}
+
+type CEName = keyof CEConfigByNameMap;
+
+type CEModifiers<Name extends CEName> = string[];
+type CETag<Name extends CEName> = string;
+
+type CEPropsByName<Name extends CEName> = CEConfigByNameMap[Name];
+
+type CEConfig<Name extends CEName> = {
+  modifiers: CEModifiers<Name>[];
+  tag: CETag<Name>;
+} & CEPropsByName<Name>
+
+type WithCEConfigByName<Name extends CEName, T> = T & {
+  [ceName: Name]: CEConfigByNameMap[Name];
+}
+
+type CEProps<Name extends CEName> = WithCEConfigByName<Name, {
+  modifiers: CEModifiers<Name>[];
+  tag: CETag<Name>;
+}>;
+
+const Props: CEProps<'text'> = {
+}
+
 const Index = () => {
   const pageData = useGetData("IndexPage");
     return (
