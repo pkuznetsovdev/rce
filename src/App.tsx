dfecbcf89b9c7832d30fb4f30ab9c8dbd44e05ed
pkuznetsovdev <kuznetsov.pavel@protonmail.com>
2023-07-08 09:54:58 +0400

    feat: add customConfig

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import './custom/project-a/styles/index.scss';
 import Router from './custom/project-a/router/router';
 import { CustomProviders } from 'src/custom';
+import CE from 'react-content-elements';
+
+const TAG_BY_ELEMENT_MODIFIER = {
+    header: 'h1',
+    section: 'section',
+    title: 'h3',
+    subtitle: 'h5',
+    ol: 'ol',
+} as const;
+
+CE.setup({
+    tags: {
+        byModifier: TAG_BY_ELEMENT_MODIFIER,
+    },
+});
 
 function App() {
   return (
